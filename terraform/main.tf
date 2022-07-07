c24f0cd7d44b6c7f0959cd321cc51ef75aaec3e3
johnbv <jotabrice28@gmail.com>
2022-07-07 10:13:22 +1000

    test main.tf on push 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
     }
   }
   backend "s3" {
@@ -10,7 +9,6 @@ terraform {
     key    = "terra.tfstate"
     region = "ap-southeast-2"
   }
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
