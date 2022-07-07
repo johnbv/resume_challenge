bb1fb2b2fe31e50319e6ee0d1dfed8f43491c6dd
johnbv <jotabrice28@gmail.com>
2022-07-07 10:25:51 +1000

    test main.tf on push 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,9 @@ terraform {
     key    = "terra.tfstate"
     region = "ap-southeast-2"
   }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region  = "ap-southeast-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-07620139298af599e"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TestEC2"
-  }
 }
